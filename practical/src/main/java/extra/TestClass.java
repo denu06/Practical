c8200e0e8c55990d3eb1f3fbf61c7d1ce4ec24e6
DenishM <denish.m@ezdi.us>
2019-12-13 16:52:41 +0530

    Update

---
+++ practical/src/main/java/extra/TestClass.java
@@ -0,0 +1,13 @@
+package extra;
+
+public class TestClass
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+		String input = "{ \"component\": \"MDM_RISK\", \"name\": \"fsdf\" }";
+	}
+
+}
